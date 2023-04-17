2b25a702fac9bb71713d5632364dd7c8c0bf827b
Vincenzofdg <vincenzofdg.dev@gmail.com>
2023-04-17 00:38:26 -0300

    feat: 100%

--- merge_docs.py
+++ merge_docs.py
@@ -6,7 +6,7 @@ tuesday = read_excel('documents/final/tuesday.xlsx')
 wednesday = read_excel('documents/final/wednesday.xlsx')
 thursday = read_excel('documents/final/thursday.xlsx')
 friday = read_excel('documents/final/friday.xlsx')
-# saturday = read_excel('documents/final/saturday.xlsx')
+saturday = read_excel('documents/final/saturday.xlsx')
 sunday = read_excel('documents/final/sunday.xlsx')
 
 to_concat = [monday, tuesday, wednesday, thursday, friday, sunday]
@@ -16,7 +16,6 @@ result = concat(to_concat, ignore_index=True)
 result.drop_duplicates(keep='first', inplace=True)
 
 result.to_excel('documents/final/all.xlsx', index=False)
-
 
 all = read_excel('documents/final/all.xlsx')
 
@@ -28,20 +27,15 @@ for person in people:
     name = person[0]
     count = 0
     duplicate = False
-
-
     for user in people:
         if (user[0] == name):
           if (count > 1):
             duplicate = True
           else:
              count += 1
-
     if (duplicate is False):
       passed.append(person)
-
     i += 1
-
 
 file = DataFrame(passed)
 file.to_excel('result.xlsx', index=True, header=['User', 'Viewers', 'Link', 'Email'])
